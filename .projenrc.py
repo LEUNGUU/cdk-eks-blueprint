2edbe0d7fac9c5c73da49346246201480480c8df
leunguu <liangy3928@gmail.com>
2023-04-30 17:34:35 +0800

    chore: add github workflow

--- .projenrc.py
+++ .projenrc.py
@@ -1,4 +1,5 @@
-from projen.python import PythonProject
+from projen.python import PythonProject, Setuptools
+from projen.release import Publisher, Release
 
 project = PythonProject(
     author_email="liangy3928@gmail.com",
@@ -6,6 +7,24 @@ project = PythonProject(
     module_name="cdk_eks_blueprint",
     name="cdk-eks-blueprint",
     version="0.1.0",
+    vscode=False,
+    auto_approve_options={"allowed_usernames": ["leunguu"]},
+    dev_deps=["pytest"],
 )
+
+package = Setuptools(
+    project=project,
+    author_email="liangy3928@gmail.com",
+    author_name="leunguu",
+    version="0.0.1",
+)
+
+publisher = Publisher(
+    project=project,
+    build_job_id="releasePypi",
+    artifact_name="dist",
+)
+
+publisher.publish_to_py_pi()
 
 project.synth()
